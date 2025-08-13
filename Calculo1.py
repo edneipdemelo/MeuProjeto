745cbbbdf17df624944d56af9e224899139996cf
Ednei P. de Melo <root@darkstar.eti.br>
2025-08-13 12:48:20 -0300

    Demonstração de operações aritméticas em Python...

---
+++ Calculo1.py
@@ -0,0 +1,7 @@
+X = int(input("Digite o valor de X:"))
+Y = int(input("Digite o valor de Y:"))
+SOMA = X + 5
+SUBTRACAO = Y - 3
+
+print("A soma do primeiro número + 5:", SOMA)
+print("A subtração do segundo número -3:", SUBTRACAO)
