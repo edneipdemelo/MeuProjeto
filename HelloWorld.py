93a5924d1f0dad4ba500b330ac7aa1c8e6823454
edneipdemelo <92800189+edneipdemelo@users.noreply.github.com>
2025-08-13 11:31:33 -0300

    Add files via upload

---
+++ HelloWorld.py
@@ -0,0 +1,5 @@
+# Este será o meu primeiro programa de computador!
+print("Olá Mundo!")
+print("I'm not the person that you think I am!")
+print("But I'm could be the person that you want.")
+print('If you think I\'m really "Special"!')
---
+++ MyProfile.py
@@ -0,0 +1,11 @@
+print("Digite o seu nome:")
+MyName = input()
+print("Digite a sua altura:")
+MyHeight = float(input())
+print("Digite a sua idade:")
+MyAge = int(input())
+
+# Exibição dos dados...
+print("Meu nome é", MyName)
+print("Minha altura é", MyHeight, "metros.")
+print("Minha idade é", MyAge, "anos.")
