8d5c786897279431e8bd98e3eaa32ca13548f4c7
Ednei P. de Melo <root@darkstar.eti.br>
2025-08-18 09:46:09 -0300

    Correção da atividades da aula passada...

---
+++ Calculo2.py
@@ -0,0 +1,5 @@
+# Calcular as prestações do produto...
+valor = int(input("Digite o valor do produto: "))
+quantidade = int(input("Digite a quantidade de parcelas: "))
+parcelas = valor / quantidade
+print("O valor da parcela será: R$", parcelas)
---
+++ Calculo3.py
@@ -0,0 +1,6 @@
+# Calcular o quadrado e o cubo de um número...
+numero = int(input("Digite o número: "))
+quadrado = numero ** 2
+cubo = numero ** 3
+print("O seu quadrado é:", quadrado)
+print("O seu cubo é:", cubo)
